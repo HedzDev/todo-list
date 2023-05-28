94f8b7744bfc0b7abb9e4cb950bb1d57813c0be3
Hedi Rivas <hedi.rivas@gmail.com>
2023-05-28 12:39:38 +0200

    updated project

--- components/Home.js
+++ components/Home.js
@@ -7,23 +7,12 @@ function Home() {
     <div className={styles.container}>
       <div className={styles.card}>
         <div className={styles.addContainer}>
-          <input
-            type="text"
-            placeholder="Task"
-            id="taskName"
-            className={styles.addTask}
-          />
+          <input type="text" placeholder="Task" className={styles.addTask} />
           <div className={styles.urgentSection}>
-            <input
-              type="checkbox"
-              id="urgent"
-              className={styles.urgentCheckbox}
-            />
+            <input type="checkbox" className={styles.urgentCheckbox} />
             <span className={styles.urgent}>URGENT</span>
           </div>
-          <button id="add" className={styles.button}>
-            ADD TASK
-          </button>
+          <button className={styles.button}>ADD TASK</button>
         </div>
 
         <div className={styles.tasksContainer}>
